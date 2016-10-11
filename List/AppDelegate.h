9079a40c427c8a0e033d373276ab7dd49d9fb046
LALALATT <lalalatt@lalalattdemac.local>
2016-10-11 12:09:38 +0800

    Initial Commit

---
+++ List/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  List
+//
+//  Created by LALALATT on 16/10/11.
+//  Copyright © 2016年 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
